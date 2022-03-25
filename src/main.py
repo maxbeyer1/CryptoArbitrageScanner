811c33a21f2fc5bdbb5e8c410583bf69fc249261
Max Beyer <max.beyer09@gmail.com>
2022-03-25 16:27:01 -0400

    Refactored into neater functions

--- src/main.py
+++ src/main.py
@@ -22,7 +22,11 @@ def get_profit(coin_data):
 
     # sort through all exchanges (with high trust scores) and add to dictionary 
     for exchange in tickers:
-        if (exchange['target'] == 'USDT' or exchange['target'] == 'USD') and exchange['trust_score'] == 'green':
+        if (
+            (exchange['target'] == 'USDT' or exchange['target'] == 'USD') and
+             exchange['trust_score'] == 'green' and
+             exchange['market']['name'] != 'eToroX' # does not allow withdraw of crypto
+        ):
             prices[str(exchange['market']['name'])] = exchange['last']
     
     # calculate hgihest and lowest from dictionary
@@ -37,51 +41,72 @@ def get_profit(coin_data):
 
     return profit, high, low
 
-coin_tickers = {}
+# returns array of top coins from CoinGecko
+def get_coin_data(numCoins):
+    coin_tickers = {}
 
-# get list of top 100 coins by market cap
-coin_list = cg.get_coins_markets(vs_currency='usd', order='market_cap_desc')
+    # get list of top 100 coins by market cap
+    coin_list = cg.get_coins_markets(vs_currency='usd', order='market_cap_desc')
 
-# add first 50 to dictionary (limited by CoinGecko API rate limits)
-for coin in coin_list[:50]:
-    id = coin['id']
-    coin_tickers[id] = cg.get_coin_ticker_by_id(id=id)
+    # add first 50 to dictionary (limited by CoinGecko API rate limits)
+    for coin in coin_list[:numCoins]:
+        id = coin['id']
+        coin_tickers[id] = cg.get_coin_ticker_by_id(id=id)
+    
+    return coin_tickers
 
-possibleTrades = {}
+def get_trades(coin_data):
+    possible_trades = {}
 
-# get most profitable trade for each coin
-for ticker in coin_tickers:
-    profit, highExchange, lowExchange = get_profit(coin_tickers[ticker])
+    # get most profitable trade for each coin
+    for ticker in coin_data:
+        profit, highExchange, lowExchange = get_profit(coin_data[ticker])
 
-    possibleTrades[ticker] = {
-        'profit': profit,
-        'highExchange': highExchange,
-        'lowExchange': lowExchange
-    }
+        possible_trades[ticker] = {
+            'profit': profit,
+            'highExchange': highExchange,
+            'lowExchange': lowExchange
+        }
+    
+    # tether is not a viable arbitrage opportunity but often shows up as one due to mispricing
+    del possible_trades['tether']
 
-max_profit_ticker = ''
-max_profit = 0
-max_profit_highExchange = ''
-max_profit_lowExchange = ''
+    return possible_trades
 
-# find most profitable trade
-for trade in possibleTrades:
-    if possibleTrades[trade]['profit'] > max_profit and trade != 'tether':
-        highExchange = possibleTrades[trade]['highExchange']
-        lowExchange = possibleTrades[trade]['lowExchange']
+# finds highest profit trade from possible trades
+def suggest_trade(possible_trades):
+    max_profit_ticker = ''
+    max_profit = 0
+    max_profit_highExchange = ''
+    max_profit_lowExchange = ''
 
-        max_profit = possibleTrades[trade]['profit']
-        max_profit_ticker = trade
-        max_profit_highExchange = highExchange
-        max_profit_lowExchange = lowExchange
+    # find most profitable trade
+    for trade in possible_trades:
+        if possible_trades[trade]['profit'] > max_profit:
 
-# tether is not a viable arbitrage opportunity but often shows up as one due to mispricing
-del possibleTrades['tether']
+            highExchange = possible_trades[trade]['highExchange']
+            lowExchange = possible_trades[trade]['lowExchange']
 
-print ("highest profit trade is with: " + max_profit_ticker + " for a profit of " + str(max_profit))
-print ("buy " + max_profit_ticker + " on " + max_profit_lowExchange + " and sell on " + max_profit_highExchange)
+            max_profit = possible_trades[trade]['profit']
+            max_profit_ticker = trade
+            max_profit_highExchange = highExchange
+            max_profit_lowExchange = lowExchange
 
-df = pd.DataFrame(possibleTrades).T
-df.fillna(0, inplace=True)
-df.sort_values(by='profit', ascending=False, inplace=True)
-print(df)
+    print ("highest profit trade is with: " + max_profit_ticker + " for a profit of " + str(max_profit))
+    print ("buy " + max_profit_ticker + " on " + max_profit_lowExchange + " and sell on " + max_profit_highExchange)
+
+# create and sort dataframe with key from dict
+def create_sorted_dataframe(data, sort_key):
+    df = pd.DataFrame(data).T
+    df.fillna(0, inplace=True)
+    df.sort_values(by=sort_key, ascending=False, inplace=True)  
+
+    return df
+
+coin_data = get_coin_data(50)
+possible_trades = get_trades(coin_data)
+suggest_trade(possible_trades)
+
+sorted_trades_df = create_sorted_dataframe(possible_trades, 'profit')
+
+print(sorted_trades_df)
